8329db938cbe8b9fa9f85a2dc13bfb623b466938
khoabd <khoabd@esollabs.com>
2023-05-07 13:06:18 +0700

    Update ssl and pass

--- add_whitelist.py
+++ add_whitelist.py
@@ -3,18 +3,35 @@ import os
 
 from web3 import Web3
 from dotenv import load_dotenv
-from rediscluster import RedisCluster
+from redis.cluster import RedisCluster, ClusterNode
 import csv
 
 load_dotenv()
 
 startup_nodes = json.loads(os.getenv('REDIS_CLUSTER', '[]'))
-PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+nodes = list()
+for node in startup_nodes:
+    nodes.append(ClusterNode(host=node['host'], port=node['port']))
 
-redis = RedisCluster(startup_nodes=startup_nodes, decode_responses=True, skip_full_coverage_check=True)
+if os.getenv('REDIS_PASSWORD'):
+    ssl_redis = bool(int(os.getenv("SSL", "0")))
+    print('ssl', ssl_redis)
+    redis = RedisCluster(startup_nodes=nodes,
+                         # host=os.getenv("REDIS_AUTH_HOST"),
+                         # port=int(os.getenv("REDIS_AUTH_PORT")),
+                         decode_responses=True,
+                         ssl=ssl_redis,
+                         skip_full_coverage_check=True,
+                         password=os.getenv('REDIS_PASSWORD'))
+else:
+    redis = RedisCluster(startup_nodes=nodes,
+                         decode_responses=True,
+                         skip_full_coverage_check=True)
 
+# address = "0x183Ff214179cd2B1c06A937D663F192340edd159".lower()
+# redis.set(f"ladys:whitelist:{address}", 1)
 _web3 = Web3()
-with open('whitelist.csv') as csv_file:
+with open('whitelist_holder.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     done_address = 0
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import json
 import os
 from web3 import Web3
-
+from redis.cluster import RedisCluster, ClusterNode
 from fastapi import FastAPI, HTTPException
 
-from rediscluster import RedisCluster
+# from rediscluster import RedisCluster
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -13,8 +13,25 @@ app = FastAPI()
 
 startup_nodes = json.loads(os.getenv('REDIS_CLUSTER', '[]'))
 PRIVATE_KEY = os.getenv("PRIVATE_KEY")
+nodes = list()
+for node in startup_nodes:
+    nodes.append(ClusterNode(host=node['host'], port=node['port']))
+print('nodes', nodes)
 
-redis = RedisCluster(startup_nodes=startup_nodes, decode_responses=True,skip_full_coverage_check=True)
+if os.getenv('REDIS_PASSWORD'):
+    ssl_redis = bool(int(os.getenv("SSL", "0")))
+    print('ssl', ssl_redis)
+    redis = RedisCluster(startup_nodes=nodes,
+                     # host=os.getenv("REDIS_AUTH_HOST"),
+                     # port=int(os.getenv("REDIS_AUTH_PORT")),
+                     decode_responses=True,
+                     ssl=ssl_redis,
+                     skip_full_coverage_check=True,
+                     password=os.getenv('REDIS_PASSWORD'))
+else:
+    redis = RedisCluster(startup_nodes=nodes,
+                     decode_responses=True,
+                     skip_full_coverage_check=True)
 _web3 = Web3()
 
 chainId = int(os.getenv('CHAIN_ID'))
